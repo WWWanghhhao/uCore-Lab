f57915c0cb9334b2de9eb91ee0318739bf2ea2c3
Knight9038 <2580306795@qq.com>
2024-10-26 11:57:05 +0800

    slub完善

--- lab2/kern/mm/slub.c
+++ lab2/kern/mm/slub.c
@@ -9,7 +9,7 @@
 struct slab_t
 {
     int ref;                     // 页的引用次数（保留）
-    struct kmem_cache_t *cachep; // 仓库对象指针
+    struct kmem_cache_t *cachep; // 指向该 Slab 所属的仓库对象指针
     uint16_t inuse;              // 已分配对象的数目
     int16_t free;                // 下一个空闲对象偏移量，也就是静态链表的头部
     list_entry_t slab_link;      // Slab链表
@@ -20,14 +20,18 @@ struct slab_t
 #define SIZED_CACHE_MIN 16
 #define SIZED_CACHE_MAX 2048
 
+//根据链表节点获取相应的 slab_t 结构体  le2page 是一个将链表节点转换为 Page 结构的函数，le 是链表项，link 是 slab_link 字段
 #define le2slab(le, link) ((struct slab_t *)le2page((struct Page *)le, link))
+//将 slab_t 转换为虚拟地址
 #define slab2kva(slab) (page2kva((struct Page *)slab))
 
-static list_entry_t cache_chain;                           // cache_chain对应的链表会链接所有kmem_cache
+static list_entry_t cache_chain;                           // 一个链表头，连接所有的 kmem_cache_t 对象 每个 kmem_cache_t 代表一个特定大小的内存缓存cache_chain对应的链表会链接所有kmem_cache
 static struct kmem_cache_t cache_cache;                    // kmem_cache_t仓库，kmem_cache_t也是由Slab算法分配的，因此它会cache所有的kmem_cache_t
+                                                            //一个特殊的 kmem_cache_t，用于缓存 kmem_cache_t 对象自身的 Slab 这是因为 kmem_cache_t 也是通过 Slab 分配的，因此它需要一个专门的缓存区
 static struct kmem_cache_t *sized_caches[SIZED_CACHE_NUM]; // 指针数组，数组的每一个元素指向1个固定大小的内置仓库，共有8个内置仓库，这些仓库存放实际的对象
+                                                            //数组中的每个元素指向一个 kmem_cache_t，每个缓存对象管理特定大小的内存块 如 16 字节、32 字节等
 static char *cache_cache_name = "cache";                   // kmem_cache_t仓库的名字
-static char *sized_cache_name = "sized";                   // 内置仓库的名字
+static char *sized_cache_name = "sized";                   // 内置仓库的名字 内置缓存（管理 16 至 2048 字节大小的内存块）
 
 /* 从buddy中申请一页内存作为完全空闲的slab
    1. 从buddy中申请一个page
@@ -43,19 +47,19 @@ kmem_cache_grow(struct kmem_cache_t *cachep)
     struct Page *page = alloc_page(); // ! 这个是manager中的函数，对接上buddy的接口
     // cprintf("从buddy拿出来b！%p\n", page);
     // cprintf("%p\n", page2pa(page));
-    // 获取页的虚拟地址，这里需要自己实现
+    // 获取页的虚拟地址，page2kva在pmm.c实现
     void *kva = page2kva(page);
     // 2. 初始化Slab元数据，初始化空闲链表bufctl
     struct slab_t *slab = (struct slab_t *)page;
-    slab->cachep = cachep;        // 设置仓库对象
-    slab->inuse = slab->free = 0; // 当前没有分配过
+    slab->cachep = cachep;        // 设置仓库对象 将 Slab 与对应的 kmem_cache_t 缓存池关联，表示该 Slab 属于哪个缓存
+    slab->inuse = slab->free = 0; // 当前没有分配过 初始化 Slab 的使用状态 inuse 代表已分配的对象数量，free 表示下一个空闲对象的偏移量
     // 在这一页开头构建静态链表（由于是初始化，直接按顺序穿起来），记录可用内存区块的偏移
     int16_t *bufctl = kva;
     for (int i = 1; i < cachep->num; i++)
         bufctl[i - 1] = i;
-    bufctl[cachep->num - 1] = -1;
+    bufctl[cachep->num - 1] = -1;  //将链表的最后一个元素设为 -1，表示链表的终止
     // 3. 初始化buf中的对象
-    // 计算静态链表后的虚拟地址
+    // 计算静态链表后的虚拟地址 将 bufctl 指针后移到 Slab 中存储对象的部分，开始对每个对象进行初始化
     void *buf = bufctl + cachep->num;
     // 有构造函数才做
     if (cachep->ctor)
@@ -66,22 +70,22 @@ kmem_cache_grow(struct kmem_cache_t *cachep)
     return slab;
 }
 
-// 释放cachep中的一个slab块对应的页，析构buf中的对象后将page归还
+/*实现了 kmem_slab_destroy 函数，用于释放一个 Slab 块对应的页内存，析构 Slab 中的对象，并将内存页归还给 Buddy 分配器*/ 
 static void
 kmem_slab_destroy(struct kmem_cache_t *cachep, struct slab_t *slab)
 {
     // 计算buf的虚拟地址
-    struct Page *page = (struct Page *)slab;
+    struct Page *page = (struct Page *)slab; //将 slab 指针转换为 Page 结构体的指针
     int16_t *bufctl = page2kva(page);
-    void *buf = bufctl + cachep->num;
-    // 有析构函数才做
+    void *buf = bufctl + cachep->num;// num每个Slab保存的对象数目
+    // 有析构函数才做 
     if (cachep->dtor)
-        // 析构每个对象
+        // 析构每个对象 
         for (void *p = buf; p < buf + cachep->objsize * cachep->num; p += cachep->objsize)
             cachep->dtor(p, cachep, cachep->objsize);
-    // 清除property和flag
-    page->property = page->flags = 0;
-    list_del(&(page->page_link));
+    
+    page->property = page->flags = 0;// 清除property和flag
+    list_del(&(page->page_link));//从相关的链表中删除 page，以确保它不再被引用或使用
     free_page(page); // ! 这个是manager中的函数，对接上buddy的接口
 }
 
@@ -148,6 +152,7 @@ check_kmem()
 
     // 创建一个测试仓库，初始化对象内存空间全为TEST_OBJECT_CTVAL
     // test_object大小为2046字节
+    //名为 test 的仓库 cp0，用于存储 test_object 类型的对象。构造和析构函数分别初始化和清理对象
     struct kmem_cache_t *cp0 = kmem_cache_create(test_object_name, sizeof(struct test_object), test_ctor, test_dtor);
     assert(cp0 != NULL);                                         // 创建成功
     assert(kmem_cache_size(cp0) == sizeof(struct test_object));  // 对象大小一致
@@ -248,16 +253,16 @@ kmem_cache_create(const char *name, size_t size,
     return cachep;
 }
 
-// 销毁kmem_cache_t及其中所有的Slab
+// 销毁kmem_cache_t及其中所有的Slab销毁一个 kmem_cache_t 缓存以及其所有关联的 slab，释放资源并清除链表中对其的引用
 void kmem_cache_destroy(struct kmem_cache_t *cachep)
 {
     list_entry_t *head, *le;
     // 销毁slabs_full
-    head = &(cachep->slabs_full);
-    le = list_next(head);
+    head = &(cachep->slabs_full);//将 head 指向 slabs_full 链表的头部
+    le = list_next(head);//通过循环遍历 slabs_full 链表，逐个调用 kmem_slab_destroy 函数销毁其中的 slab，释放内存
     while (le != head)
     {
-        list_entry_t *temp = le;
+        list_entry_t *temp = le;//使用 temp 临时保存当前节点，销毁后指向下一个节点，直到回到头部
         le = list_next(le);
         kmem_slab_destroy(cachep, le2slab(temp, page_link));
     }
@@ -293,33 +298,35 @@ void *
 kmem_cache_alloc(struct kmem_cache_t *cachep)
 {
     // cprintf("开始分配！\n");
-    list_entry_t *le = NULL;
+    list_entry_t *le = NULL; //定义一个链表指针 le，用于指向当前可以分配对象的 slab
     // 1. 先查找slabs_partial
     if (!list_empty(&(cachep->slabs_partial)))
         le = list_next(&(cachep->slabs_partial));
-    // 再查找slabs_free
+    // 若 slabs_partial 为空，则检查 slabs_free，其中包含全空的 slab
     else
     {
-        // 2. 还是没找到就从buddy中申请一个新的slab，对应于kmem_cache_grow函数
+        // 2. 如果 slabs_free 也为空，则调用 kmem_cache_grow 从 buddy 系统中申请一个新的 slab 并初始化
         if (list_empty(&(cachep->slabs_free)) && kmem_cache_grow(cachep) == NULL)
-            // 实在是分配不了了
+            // 如果 kmem_cache_grow 也失败，说明分配内存不足，返回 NULL
             return NULL;
         // 申请之后slabs_free就不是空了，所以是能取出来的
         le = list_next(&(cachep->slabs_free));
     }
     // 3. 分配内存，并更新各个链表与记录
-    // 先把这一块slab摘下来
+    // 先把这一块slab摘下来 从 slabs_partial 或 slabs_free 中取出一个 slab，解除与链表的连接
     list_del(le);
     // 把这个list_entry转为page，再转为slab块
     struct slab_t *slab = le2slab(le, page_link);
     // 转为虚拟地址
     void *kva = slab2kva(slab);
+    //bufctl 指向对象的空闲链表
     int16_t *bufctl = kva;
     // 偏移到buf
     void *buf = bufctl + cachep->num;
     // 从buf根据free偏移到第一个还能申请来存放对象的地址
     void *objp = buf + slab->free * cachep->objsize;
     // 取下这片区域，标记为使用，更新slab的空闲表
+    //增加 inuse 计数，标记新分配的对象，并将 free 指向下一个空闲对象
     slab->inuse++;
     slab->free = bufctl[slab->free];
     // 4. 从slab分配一个对象后，如果slab变满，那么将slab加入slabs_full
@@ -335,8 +342,8 @@ kmem_cache_alloc(struct kmem_cache_t *cachep)
 void *
 kmem_cache_zalloc(struct kmem_cache_t *cachep)
 {
-    void *objp = kmem_cache_alloc(cachep);
-    memset(objp, 0, cachep->objsize);
+    void *objp = kmem_cache_alloc(cachep);//调用 kmem_cache_alloc 函数从 cachep 指向的缓存中分配一个对象 分配成功后，objp 指向分配的对象内存地址
+    memset(objp, 0, cachep->objsize); //使用 memset 函数将 objp 指向的内存区域初始化为零 cachep->objsize 表示对象的大小，确保对象的所有字节被置为零
     return objp;
 }
 
@@ -347,18 +354,18 @@ kmem_cache_zalloc(struct kmem_cache_t *cachep)
 void kmem_cache_free(struct kmem_cache_t *cachep, void *objp)
 {
     // 获取对象所在的slab
-    void *base = page2kva(pages);
-    void *kva = ROUNDDOWN(objp, PGSIZE);
-    struct slab_t *slab = (struct slab_t *)&pages[(kva - base) / PGSIZE];
+    void *base = page2kva(pages); //获取页数组 pages 的起始虚拟地址 base
+    void *kva = ROUNDDOWN(objp, PGSIZE); //获取对象所在页的基地址 kva
+    struct slab_t *slab = (struct slab_t *)&pages[(kva - base) / PGSIZE]; //将 kva 转换为 pages 中相应的 Slab 元数据地址，通过页索引来定位 Slab
     // 获取对象在slab中的偏移
-    int16_t *bufctl = kva;
-    void *buf = bufctl + cachep->num;
-    int offset = (objp - buf) / cachep->objsize;
+    int16_t *bufctl = kva; //bufctl 指向 Slab 中管理空闲对象链表的区域
+    void *buf = bufctl + cachep->num; //buf 表示 Slab 中第一个对象的起始地址
+    int offset = (objp - buf) / cachep->objsize; 
     // 更新Slab元信息
-    list_del(&(slab->slab_link)); // 在链表中删除
-    bufctl[offset] = slab->free;
-    slab->inuse--;
-    slab->free = offset;
+    list_del(&(slab->slab_link)); // 将当前 Slab 从原来的链表（slabs_partial 或 slabs_full）中删除
+    bufctl[offset] = slab->free; //将对象插入空闲链表中
+    slab->inuse--; //更新使用计数
+    slab->free = offset; //更新空闲链表头部
     // 如果Slab变空，那么将Slab加入slabs_free链表，否则加入slabs_partial
     if (slab->inuse == 0)
         list_add(&(cachep->slabs_free), &(slab->slab_link));
@@ -383,20 +390,20 @@ kmem_cache_name(struct kmem_cache_t *cachep)
 // 释放仓库的slabs_free链表中所有Slab
 int kmem_cache_shrink(struct kmem_cache_t *cachep)
 {
-    int count = 0;
+    int count = 0; //用于记录释放的 Slab 数量
     list_entry_t *le = list_next(&(cachep->slabs_free));
-    while (le != &(cachep->slabs_free))
+    while (le != &(cachep->slabs_free)) //遍历 slabs_free 链表中每一个 Slab，直到回到链表头部
     {
         list_entry_t *temp = le;
         le = list_next(le);
-        kmem_slab_destroy(cachep, le2slab(temp, page_link));
+        kmem_slab_destroy(cachep, le2slab(temp, page_link)); //kmem_slab_destroy 用于释放 temp 所指向的 Slab 页面，并将其归还到物理内存分配器
         count++;
     }
     return count;
 }
 
 /* 释放所有的全空闲slab
-   遍历仓库链表，对每一个仓库进行kmem_cache_shrink操作
+   遍历cache_chain仓库链表，对每一个仓库进行kmem_cache_shrink操作
 */
 int kmem_cache_reap()
 {
@@ -415,24 +422,26 @@ kmalloc(size_t size)
     return kmem_cache_alloc(sized_caches[kmem_sized_index(size)]);
 }
 
-// 释放内置仓库对象
+// 找到对象所在的 Slab 并调用相应缓存的释放函数，释放对象的内存
 void kfree(void *objp)
 {
     void *base = slab2kva(pages);
     void *kva = ROUNDDOWN(objp, PGSIZE);
-    struct slab_t *slab = (struct slab_t *)&pages[(kva - base) / PGSIZE];
-    kmem_cache_free(slab->cachep, objp);
+    struct slab_t *slab = (struct slab_t *)&pages[(kva - base) / PGSIZE]; //根据页面起始地址找到 objp 所在的 Slab，并将该页面结构体转换为 slab_t 类型
+    kmem_cache_free(slab->cachep, objp); //通过 Slab 的缓存指针 cachep 调用 kmem_cache_free 函数，释放该对象的内存
 }
 
-/* 1. 初始化kmem_cache_t仓库：由于kmem_cache_t也是由Slab算法分配的，所以需要预先手动初始化一个kmem_cache_t仓库
-   2. 初始化内置仓库：初始化8个固定大小的内置仓库
+/* 1. 初始化kmem_cache_t仓库 由于kmem_cache_t也是由Slab算法分配的，所以需要预先手动初始化一个kmem_cache_t仓库
+   2. 初始化内置仓库  初始化8个固定大小的内置仓库
    3. 测试slub分配器
 */
 void kmem_int()
 {
 
     // 1. 初始化kmem_cache的cache仓库，简称为cache_cache
-    cache_cache.objsize = sizeof(struct kmem_cache_t);
+    /*cache_cache 是专门用于分配 kmem_cache_t 对象的缓存，kmem_cache_t 是 Slab 分配器中缓存对象的结构体
+    为了支持 Slab 分配器正常运行，需要手动初始化一个专用于 kmem_cache_t 的缓存（即 cache_cache）*/
+    cache_cache.objsize = sizeof(struct kmem_cache_t); //指定对象大小为 kmem_cache_t 结构体的大小
     cache_cache.num = PGSIZE / (sizeof(int16_t) + sizeof(struct kmem_cache_t)); // 算num的时候还要考虑前面int16_t的信息
     cache_cache.ctor = NULL;
     cache_cache.dtor = NULL;
@@ -444,12 +453,12 @@ void kmem_int()
 
     // 初始化cache_chain（这个链表会串起来所有的kmem_cache_t）
     list_init(&(cache_chain));
-    // 将第一个kmem_cache_t也就是cache_cache加入到cache_chain对应的链表中
+    // 将第一个kmem_cache_t也就是cache_cache加入到cache_chain对应的链表中 作为第一个缓存
     list_add(&(cache_chain), &(cache_cache.cache_link));
 
     // 2. 初始化8个固定大小的内置仓库
-    for (int i = 0, size = 16; i < SIZED_CACHE_NUM; i++, size *= 2)
-        sized_caches[i] = kmem_cache_create(sized_cache_name, size, NULL, NULL);
+    for (int i = 0, size = 16; i < SIZED_CACHE_NUM; i++, size *= 2)//for 循环从大小为 16 开始，递增一倍，直至总共初始化 8 个大小不同的内置缓存
+        sized_caches[i] = kmem_cache_create(sized_cache_name, size, NULL, NULL); //为每个大小创建一个缓存，用于内核分配内存对象
 
     // 3. 进行测试
     check_kmem();
--- lab2/kern/mm/slub.h
+++ lab2/kern/mm/slub.h
@@ -20,18 +20,30 @@ struct kmem_cache_t
     list_entry_t cache_link;                             // 仓库链表
 };
 
+/*kmem_cache_create创建一个新的缓存池
+name 是缓存池的名字 size 是每个对象的大小 ctor 是对象的构造函数 dtor 是对象的析构函数
+它返回一个指向 kmem_cache_t 结构的指针，表示新创建的缓存池*/
 struct kmem_cache_t *
 kmem_cache_create(const char *name, size_t size,
                   void (*ctor)(void *, struct kmem_cache_t *, size_t),
                   void (*dtor)(void *, struct kmem_cache_t *, size_t));
+//kmem_cache_destroy销毁一个缓存池，释放所有资源
 void kmem_cache_destroy(struct kmem_cache_t *cachep);
+//kmem_cache_alloc从指定的缓存池中分配一个对象
 void *kmem_cache_alloc(struct kmem_cache_t *cachep);
+//kmem_cache_zalloc类似 kmem_cache_alloc，但分配的内存被清零
 void *kmem_cache_zalloc(struct kmem_cache_t *cachep);
+//kmem_cache_free释放从缓存池中分配的对象
 void kmem_cache_free(struct kmem_cache_t *cachep, void *objp);
+//kmem_cache_size返回缓存池中每个对象的大小
 size_t kmem_cache_size(struct kmem_cache_t *cachep);
+//kmem_cache_name返回缓存池的名字
 const char *kmem_cache_name(struct kmem_cache_t *cachep);
+//kmem_cache_shrink收缩缓存池，释放空闲的 slab 以节省内存
 int kmem_cache_shrink(struct kmem_cache_t *cachep);
+//kmem_cache_reap回收整个系统中所有的缓存池，释放可以回收的内存
 int kmem_cache_reap();
+//kmalloc 和 kfree类似标准库中的 malloc 和 free，用于在 SLUB 系统中分配和释放内存
 void *kmalloc(size_t size);
 void kfree(void *objp);
 // size_t ksize(void *objp);
