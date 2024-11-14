a413db856d6f4228068d9d3b24602c9e0e1ef000
Wang Hao <3207834719@qq.com>
2024-11-14 18:00:49 +0800

    fix lru bugs

---
+++ lab3/obj/kern/mm/swap_lru.d
@@ -0,0 +1,6 @@
+obj/kern/mm/swap_lru.o obj/kern/mm/swap_lru.d: kern/mm/swap_lru.c \
+ kern/mm/memlayout.h libs/defs.h libs/atomic.h libs/list.h kern/mm/pmm.h \
+ kern/debug/assert.h kern/mm/memlayout.h kern/mm/mmu.h kern/mm/vmm.h \
+ kern/sync/sync.h kern/driver/intr.h libs/riscv.h libs/stdio.h \
+ libs/stdarg.h libs/string.h kern/mm/swap.h kern/mm/pmm.h kern/mm/vmm.h \
+ kern/mm/swap_lru.h
